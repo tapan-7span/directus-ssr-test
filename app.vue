f6f62b24148322f5a5b9b1e2780fa0a09de45cae
tapan-7span <tapan@7span.com>
2024-08-16 18:48:51 +0530

    Working done

--- app.vue
+++ app.vue
@@ -1,47 +1,12 @@
-<template>
-  <ClientOnly>
-    <hr />
-    <div v-if="data">
-      <p>Host Data:</p>
-      {{ data }}
-    </div>
-    <hr />
-    <div v-if="error">
-      <p>Error:</p>
-      {{ error }}
-    </div>
-    <hr />
-    <div v-if="error">
-      <p>Status :</p>
-      <strong>
-        {{ error?.statusCode }}
-      </strong>
-      <p>
-        {{ error?.cause }}
-      </p>
-    </div>
-    <hr />
-  </ClientOnly>
-</template>
+<template>Hello Wrold</template>
 <script setup>
-const { data, error, status } = await useFetch(
-  "https://craft.alike.host/items/languages/?fields=code&fields=name&fields=dir"
-);
-
-if (error?.value) {
-  console.log("Error : ", error.value);
-  console.log("Status Code : ", error.value?.statusCode);
-}
-
-if (data?.value) {
-  console.log("Response : ", data.value);
-}
-
-if (status?.value) {
-  console.log("status : ", status.value);
-}
+const { data, error } = await useFetch("https://craft.alike.io/items/faqs");
 
 onMounted(async () => {
+  if (error?.value) {
+    console.log("Error : ", error.value);
+    console.log("Status Code : ", error.value?.statusCode);
+  }
   console.log("data", data.value);
 });
 </script>
